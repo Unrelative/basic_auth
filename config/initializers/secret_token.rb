f86cec491ac6ab16716e937cd03ba0cf3e2f49e0
Paul Burris <paul@touchin2.com>
2014-01-21 21:30:45 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BasicAuth::Application.config.secret_token = 'f8fe29460f59f3286f525c8ae596604e3fa67dae45efce57dc239e3ab988dfb2f56e93c9212b46c282615651debf2f73a44347232517a774e2a49a5df8625b4a'
